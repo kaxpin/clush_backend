8af1500c6169332059b00bc5757e550979c09921
Seyeon Sim <simsey97@naver.com>
2024-12-12 01:47:56 +0900

    디렉토리 생성

---
+++ src/main/java/com/expample/clush_backend/ClushBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expample.clush_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClushBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expample/clush_backend/ClushBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expample.clush_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClushBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
